e3944f6cf4f10bd362a82858b82f2c6416648e5c
unknown <wathekbel11@gmail.com>
2024-04-19 09:19:24 +0100

    intialization

---
+++ src/main/java/com/example/wbcorps/ecom/EComApplication.java
@@ -0,0 +1,13 @@
+package com.example.wbcorps.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wbcorps/ecom/EComApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wbcorps.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
